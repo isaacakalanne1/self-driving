e34f016bac26358625c09e9b57e75cfb31576908
Isaac Akalanne <iakalann@jaguarlandrover.com>
2022-09-16 16:43:36 +0100

    Updated road divider to be transparent

--- Assets/Scripts/DriveToGoalAgent.cs
+++ Assets/Scripts/DriveToGoalAgent.cs
@@ -40,7 +40,6 @@ public class DriveToGoalAgent : Agent
 
         if (DidDriveOffRoad())
         {
-            Debug.Log("Drove off road!");
             SetReward(-10000f);
             EndEpisode();
         }
